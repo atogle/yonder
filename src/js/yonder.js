82a7cdf1ed82c2c6b4376d8be51d3e64363ec899
Aaron Ogle <atogle@gmail.com>
2011-12-27 22:11:50 -0500

    add namespace, remove example map code

--- src/js/yonder.js
+++ src/js/yonder.js
@@ -1,45 +1,15 @@
-var map = new L.Map('map');
-		
-var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/fa2b7d4057c846daa7691c7995240bd5/997/256/{z}/{x}/{y}.png',
-	cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-	cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttribution});
+$(function(){
+	var initMap = function() {
+		var map = new L.Map('map');
+				
+		var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/fa2b7d4057c846daa7691c7995240bd5/997/256/{z}/{x}/{y}.png',
+			cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttribution});
 
-map.setView(new L.LatLng(51.505, -0.09), 13).addLayer(cloudmade);
+		map.setView(new L.LatLng(0, 0), 2).addLayer(cloudmade);
+	};
 
+	initMap();
 
-var markerLocation = new L.LatLng(51.5, -0.09),
-	marker = new L.Marker(markerLocation);
-
-map.addLayer(marker);
-marker.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-
-var circleLocation = new L.LatLng(51.508, -0.11),
-	circleOptions = {color: '#f03', opacity: 0.7},
-	circle = new L.Circle(circleLocation, 500, circleOptions);
-
-circle.bindPopup("I am a circle.");
-map.addLayer(circle);
-
-
-var p1 = new L.LatLng(51.509, -0.08),
-	p2 = new L.LatLng(51.503, -0.06),
-	p3 = new L.LatLng(51.51, -0.047),
-	polygonPoints = [p1, p2, p3],
-	polygon = new L.Polygon(polygonPoints);
-
-polygon.bindPopup("I am a polygon.");
-map.addLayer(polygon);
-
-
-map.on('click', onMapClick);
-
-var popup = new L.Popup();
-		
-function onMapClick(e) {
-	var latlngStr = '(' + e.latlng.lat.toFixed(3) + ', ' + e.latlng.lng.toFixed(3) + ')';
-	
-	popup.setLatLng(e.latlng);
-	popup.setContent("You clicked the map at " + latlngStr);
-	map.openPopup(popup);
-}
+	Yonder.listView = new Yonder.GeocoderListView();
+});
